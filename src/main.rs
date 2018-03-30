fad72dcd51b7adfb3964c3c13000c8c2c3313136
Jan Schulte <hello@unexpected-co.de>
2018-03-30 20:36:55 +0200

    feat: configure clap to provide a basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-  println!("Hello, world!");
+  let matches = App::new("Watson CLI")
+                        .version("1.0")
+                        .author("Jan Schulte <hello@unexpected-co.de>")
+                        .about("Command line interface for watsond")
+                        .arg(Arg::with_name("server")
+                              .short("s")
+                              .long("server")
+                              .value_name("SERVER")
+                              .help("Sets a custom server")
+                              .takes_value(true))
+                        .arg(Arg::with_name("INPUT")
+                              .help("Sets the input file to use")
+                              .required(true)
+                              .index(1))
+                        .get_matches();
+
+  let server = matches.value_of("server").unwrap_or("http://localhost:8000");
+  println!("Value for server: {}", server);
+
+  println!("Using input file: {}", matches.value_of("INPUT").unwrap());
 }
